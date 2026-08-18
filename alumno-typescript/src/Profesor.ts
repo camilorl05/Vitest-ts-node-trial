bbaebd00d93474819895eb18e17baf2afa2630f5
camilorl05 <camilorl395@gmail.com>
2026-08-18 17:36:54 -0300

    feat: add Alumno, Materia, and Profesor classes with corresponding tests

---
+++ alumno-typescript/src/Profesor.ts
@@ -0,0 +1,10 @@
+export class Profesor {
+  constructor(
+    public nombre: string,
+    public legajo: number
+  ) {}
+
+  
+  }
+
+
--- alumno-typescript/tests/Alumno.test.ts
+++ alumno-typescript/tests/Alumno.test.ts
@@ -4,22 +4,20 @@ import { Alumno } from "../src/Alumno";
 describe("Alumno", () => {
 
   test("un alumno de 18 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Juan", 18);
+    const alumno = new Alumno("Juan", 18, 55321);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
   test("un alumno de 25 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Ana", 25);
+    const alumno = new Alumno("Ana", 25, 13356);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
   test("un alumno de 19 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Pedro", 19);
+    const alumno = new Alumno("Pedro", 19, 12345);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
-
-});
-
+})
