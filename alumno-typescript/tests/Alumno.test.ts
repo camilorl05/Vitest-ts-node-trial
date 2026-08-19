132da5821494816070e37b5d83f3391d7a8958dd
camilorl05 <camilorl395@gmail.com>
2026-08-19 17:13:10 -0300

    fix: corregir errores tipográficos en los mensajes de prueba de la clase Alumno y agregar casos de alumno

--- alumno-typescript/tests/Alumno.test.ts
+++ alumno-typescript/tests/Alumno.test.ts
@@ -3,24 +3,48 @@ import { Alumno } from "../src/Alumno";
 
 describe("Alumno", () => {
 
-  test("un alumno de 18 años debe ser mayor de edad y tener numero de legajo", () => {
+  test("un alumno de 18 anios debe ser mayor de edad y tener numero de legajo", () => {
     const alumno = new Alumno("Juan", 18, 55321);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
     expect(alumno.legajo).toBe(55321);
   });
 
-  test("un alumno de 25 años debe ser mayor de edad y tener numero de legajo", () => {
+  test("un alumno de 25 anios debe ser mayor de edad y tener numero de legajo", () => {
     const alumno = new Alumno("Ana", 25, 13356);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
     expect(alumno.legajo).toBe(13356);
   });
 
-  test("un alumno de 19 años debe ser mayor de edad y tener numero de legajo", () => {
+  test("un alumno de 19 anios debe ser mayor de edad y tener numero de legajo", () => {
     const alumno = new Alumno("Pedro", 19, 12345);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
     expect(alumno.legajo).toBe(12345);
+  });
+
+  test("un alumno de 16 anios debe ser menor de edad y tener un numero de legajo", () => {
+    const alumno = new Alumno("Jazmin", 16, 47329);
+    expect(alumno.esMayorDeEdad()).toBe(false);
+    expect(alumno.legajo).toBe(47329);
+  });
+
+  test("un alumno de 15 anios debe ser menor de edad y tener un numero de legajo", () => {
+    const alumno = new Alumno("Lucia", 15, 98765);
+    expect(alumno.esMayorDeEdad()).toBe(false);
+    expect(alumno.legajo).toBe(98765);
+  });
+
+  test("un alumno de 17 anios debe ser menor de edad y tener un numero de legajo", () => {
+    const alumno = new Alumno("Tomas", 17, 18765);
+    expect(alumno.esMayorDeEdad()).toBe(false);
+    expect(alumno.legajo).toBe(18765);
+  });
+
+  test("un alumno de 24 anios debe ser mayor de edad y tener un numero de legajo", () => {
+    const alumno = new Alumno("Lucia", 24, 28765);
+    expect(alumno.esMayorDeEdad()).toBe(true);
+    expect(alumno.legajo).toBe(28765);
   });
 })
